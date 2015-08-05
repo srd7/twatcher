845ec66f94c3350c3f0775e4d956d1f55c87f4f4
srd7 <kuroneko0011@gmail.com>
2015-08-05 15:13:33 +0900

    add id to script

--- app/models/Script.scala
+++ app/models/Script.scala
@@ -3,12 +3,14 @@ package twatcher.models
 import slick.driver.H2Driver.api._
 
 case class Script(
-  path: String
+  id  : Int
+, path: String
 )
 
 class Scripts(tag: Tag) extends Table[Script](tag, "SCRIPT") {
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def path = column[String]("PATH")
-  def * = (path) <> (Script.apply _, Script.unapply)
+  def * = (id, path) <> (Script.tupled, Script.unapply)
 }
 
 object Scripts extends TableQuery(new Scripts(_)) {

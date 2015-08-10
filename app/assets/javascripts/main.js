4a04120116e547c369c92260cc201ac3b2236192
srd7 <kuroneko0011@gmail.com>
2015-08-10 18:13:40 +0900

    quit using ES6
    I noticed that I need node.js to use sbt-traceur

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,18 @@
-(($) => {
-  const doms = $(".account-form");
-  const userIds = doms.map((i, dom) => parseInt($(dom).attr("data-user-id"), 10)).toArray();
-  userIds.forEach((userId) => {
+(function ($) {
+  var doms = $(".account-form");
+  var userIds = doms.map(function (i, dom){ return parseInt($(dom).attr("data-user-id"), 10); }).toArray();
+  userIds.forEach(function (userId) {
     $.ajax(router.twatcher.controllers.SettingController.checkAccount(userId))
-      .done((data) => {
+      .done(function (data) {
         // update *icon_url, *screen_name
-        const dom = $(`.account-form[data-user-id=${data.userId}]`);
+        var dom = $(`.account-form[data-user-id=${data.userId}]`);
         $(dom).find(".account-img").attr("src", data.imageUrl);
         $(dom).find(".account-screen-name").text(`@${data.screenName}`);
         $(dom).find(".auth-check").hide();
         $(dom).find(".auth-ok").show();
       })
-      .fail((err) => {
-        const dom = $(`.account-form[data-user-id=${err.responseJSON.userId}]`);
+      .fail(function (err) {
+        var dom = $(`.account-form[data-user-id=${err.responseJSON.userId}]`);
         $(dom).find(".auth-check").hide();
         $(dom).find(".auth-failed").show();
       });
